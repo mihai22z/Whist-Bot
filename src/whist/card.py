4803b73a9f1fa91b19df25c1230257faa3b5d6b6
mihai22z <mihail.zamfirescu@yahoo.com>
2024-02-26 17:33:37 +0200

    Add Deck class and refactor Card class

--- src/whist/card.py
+++ src/whist/card.py
@@ -1,40 +1,77 @@
 class Card:
-    suits = ["Hearts",
-             "Spades",
-             "Diamonds",
-             "Clubs"]
+    """
+    Represents a card in a deck for the Whist game, with suit and value attributes.
 
-    # First two elements, have the value None so that each card value corresponds to the values tuple index
-    # Same applies for 11, since it is not part of the Whist game
-    values = ["None", "None", "2",
-              "3", "4", "5", "6",
-              "7", "8", "9", "10",
-              "None", "Jack", "Queen",
-              "King", "Ace"]
+    Attributes:
+        suits (list of str): Class variable that defines the suit names.
+        values (list of str) Class variable that defines the card values, with 'None' placeholders for indices 0, 1 and
+        11 to align the card values with their indices.
+
+    Parameters:
+        v (int): The value index of the card. Must be between 2 and 14, excluding 11, which is not used in the game.
+        s (int): The suit index of the card. Must be between 0 and 3, corresponding to the suits list.
+
+    Raises:
+        ValueError: If 'v' is not in the allowed range or if 's' is not between 0 and 3 (inclusive).
+    """
+    suits = ["Hearts", "Spades", "Diamonds", "Clubs"]
+    values = ["None", "None", "2", "3", "4", "5", "6", "7", "8", "9", "10", "None", "Jack", "Queen", "King", "Ace"]
 
     def __init__(self, v, s):
-        # value and suit are ints
         if v < 2 or v > 14 or v == 11:
             raise ValueError("Invalid card value")
         if s < 0 or s > 3:
-            raise ValueError("Invalid card value")
+            raise ValueError("Invalid suit index")
         self.value = v
         self.suit = s
 
     def __lt__(self, c2):
+        """
+        Less than comparison for sorting cards by value.
+
+        Parameters:
+            c2 (Card): Another card to compare to.
+
+        Returns:
+            bool: True if this card's value is less than the value of c2, False otherwise.
+        """
         if self.value < c2.value:
             return True
         else:
             return False
 
     def __gt__(self, c2):
+        """
+        Greater than comparison for sorting cards by value.
+
+        Parameters:
+            c2 (Card): Another card to compare to.
+
+        Returns:
+            bool: True if this card's value is greater than the value of c2, False otherwise.
+        """
         if self.value > c2.value:
             return True
         else:
             return False
 
     def __eq__(self, c2):
+        """
+        Equality comparison based on both value and suit.
+
+        Parameters:
+            c2 (Card): Another card to compare to.
+
+        Returns:
+            bool: True if both the value and suit of the cards are equal, False otherwise.
+        """
         return self.value == c2.value and self.suit == c2.suit
 
     def __repr__(self):
+        """
+        Provides a human-readable string representation of the Card instance.
+
+        Returns:
+            str: A string in the form 'Value of Suit', representing the card.
+        """
         return f"{self.values[self.value]} of {self.suits[self.suit]}"
---
+++ src/whist/deck.py
@@ -0,0 +1,59 @@
+from card import Card
+from random import shuffle
+
+
+class Deck:
+    """
+    Represents a deck of playing cards for the Whist game. The deck is adjusted based on the number of players and
+    is capable of setting a trump suit and allowing cards to be drawn.
+
+    Attributes:
+        trump (int): The suit of the trump card, initially None.
+        trump_card (Card): The card is chosen as the trump, initially None.
+        cards (list of Card): The list of cards in the deck.
+    """
+
+    def __init__(self, players):
+        """
+        Initializes the deck with cards appropriate for the number of players.
+
+        Parameters:
+            players (int): The number of players playing the game.
+
+        Raises:
+            ValueError: If the number of players is not between 3 and 6 (inclusive).
+        """
+        self.trump = None
+        self.trump_card = None
+        if players < 3 or players > 6:
+            raise ValueError("Invalid number of players")
+        self.cards = [Card(i, j) for i in range(2, 15) if i != 11 for j in range(4)]
+        shuffle(self.cards)
+
+    def set_trump(self):
+        """
+        Sets the trump suit by drawing the last card from the deck and revealing it to all players.
+        The drawn card is set aside as the trump card.
+
+        Raises:
+            RuntimeError: If the deck is empty when attempting to set the trump.
+        """
+        if len(self.cards) == 0:
+            raise RuntimeError("Cannot set trump from an empty deck")
+        self.trump_card = self.cards.pop()
+        self.trump = self.trump_card.suit
+        print("The trump card is " + str(self.trump_card))
+
+    def draw(self):
+        """
+        Draws the top card from the deck.
+
+        Returns:
+            Card: The card drawn from the top of the deck.
+
+        Raises:
+            RuntimeError: If the deck is empty when attempting to draw a card.
+        """
+        if len(self.cards) == 0:
+            raise RuntimeError("Cannot draw from an empty deck")
+        return self.cards.pop()
